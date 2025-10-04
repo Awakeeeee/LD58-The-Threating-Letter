c6dd372bf7d28b4dddd7da92937b505517308ad5
lolicoli <lolicoli@163.com>
2025-10-04 22:15:40 +0800

    导入一个UIEffect的包 给选游戏环节交互 加一些UIEffect
    打包不出来的话就不用这个做法了

--- Assets/Code/UISelectCover/SelectCoverProp.cs
+++ Assets/Code/UISelectCover/SelectCoverProp.cs
@@ -1,11 +1,14 @@
+using Coffee.UIEffects;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SelectCoverProp : MonoBehaviour
+public class SelectCoverProp : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Image coverImage;
+    public UIEffect effectOnCoverImage;
     public Image shadowImage;
 
     public void Init(ImagePreprocessData inputData)
@@ -14,5 +17,24 @@ public class SelectCoverProp : MonoBehaviour
         coverImage.SetNativeSize();
         shadowImage.sprite = inputData.image;
         shadowImage.SetNativeSize();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        effectOnCoverImage.colorFilter = ColorFilter.MultiplyAdditive;
+        effectOnCoverImage.colorIntensity = 1;
+        //effectOnCoverImage.edgeMode = EdgeMode.Plain;
+        effectOnCoverImage.shadowMode = ShadowMode.Outline8;
+
+        transform.localScale = Vector3.one * 1.05f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        effectOnCoverImage.colorFilter = ColorFilter.None;
+        //effectOnCoverImage.edgeMode = EdgeMode.None;
+        effectOnCoverImage.shadowMode = ShadowMode.None;
+
+        transform.localScale = Vector3.one;
     }
 }
