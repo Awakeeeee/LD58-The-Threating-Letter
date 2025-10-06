55b304386c2973c7e01067958088c687347c0b81
lolicoli <lolicoli@163.com>
2025-10-06 13:16:41 +0800

    漏了

--- Assets/Code/UICollage/UICollage.cs
+++ Assets/Code/UICollage/UICollage.cs
@@ -1,18 +1,47 @@
+using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UICollage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject letterPaper;
+    public Transform letterInScreenTrans;
+    public Transform letterOutOfScreenTrans;
+
+    private void OnEnable()
     {
-        
+        letterPaper.gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        letterPaper.gameObject.SetActive(false);
+    }
+
+    public void InitAnim()
+    {
+        //letterPaper.transform.position = letterOutOfScreenTrans.position;
+    }
+
+    public void OnEnterAnim(Action callback = null)
+    {
+        callback?.Invoke();
+
+        //letterPaper.transform.DOMove(letterInScreenTrans.position, 0.5f).onComplete += () =>
+        //{
+        //    callback?.Invoke();
+        //};
+    }
+
+    public void OnExitAnim(Action callback = null)
+    {
+        callback?.Invoke();
+
+        //letterPaper.transform.DOMove(letterOutOfScreenTrans.position, 0.5f).onComplete += () =>
+        //{
+        //    callback?.Invoke();
+        //};
     }
 }
